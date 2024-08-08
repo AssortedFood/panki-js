bda2bce2bd6278b237c833e65a6eb048b2e9bf64
Oxi - Pi <Hamoon.B.94@gmail.com>
2024-08-08 15:42:05 +0100

    some js example

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
-// Serve static files from the "src" directory
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,37 @@
-const App = () => {
-  const element = document.createElement('div');
-  element.innerHTML = '<h1>I am faster than you, Chris!</h1>';
-  return element;
-};
+export function loadApp() {
+  const leftHalfContent = getLeftHalfContent('https://example.com/page.html'); // Placeholder URL from SQLite
+  loadLeftHalf(leftHalfContent);
 
-export default App;
+  const rightHalfContent = getRightHalfContent('Some text from SQLite', 'Submit');
+  updateRightHalf(rightHalfContent);
+}
+
+export function loadLeftHalf(content) {
+  const leftHalf = document.getElementById('left-half');
+  leftHalf.innerHTML = content;
+}
+
+export function updateRightHalf(content) {
+  const rightHalf = document.getElementById('right-half');
+  rightHalf.innerHTML = content;
+
+  const submitButton = rightHalf.querySelector('button');
+  submitButton.addEventListener('click', () => {
+    const newRightHalfContent = getRightHalfContent('New text from SQLite', 'Back');
+    updateRightHalf(newRightHalfContent);
+  });
+}
+
+function getLeftHalfContent(url) {
+  return `
+    <iframe src="${url}" width="100%" height="100%"></iframe>
+  `;
+}
+
+function getRightHalfContent(topText, buttonText) {
+  return `
+    <div style="height: 40%;">${topText}</div>
+    <input type="text" style="height: 40%; width: 100%;" />
+    <button style="width: 100%;">${buttonText}</button>
+  `;
+}
